23b375be378793b14c996e2905b4fb90ee452b64
Fabius Gasber <fabiusgasber@gmail.com>
2024-11-29 13:04:32 +0100

    feat: implement function to process weather data

--- src/weatherservice.js
+++ src/weatherservice.js
@@ -1,13 +1,41 @@
 export const weatherservice = (() => {
     async function getJSONData (location) {
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?IconSet=icons1&key=W8SRDE35NC9EZSAB2UEM3ZD9H`);
-        if(!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        return await response.json();
-    }
-    catch (e){
+  function processWeather(json) {
+    if (!json || !(json instanceof Promise))
+      throw new Error("Data error! Unexpected data format");
+    return json
+      .then((data) => {
+        if (!data || !data.days || !data.days[0])
+          throw new Error("Data error! Unexpected data format");
+        const today = data.days[0];
+        const weatherInfo = {
+          temperature: {
+            temp: today.temp,
+            tempmax: today.tempmax,
+            tempmin: today.tempmin,
+            feelslike: today.feelslike,
+          },
+          wind: {
+            winddir: today.winddir,
+            windgust: today.windgust,
+            windspeed: today.windspeed,
+          },
+          sun: {
+            sunrise: today.sunrise,
+            sunset: today.sunset,
+            uv: today.uvindex,
+          },
+          conditions: today.conditions,
+          precipprob: today.precipprob,
+          icon: today.icon,
+        };
+        return weatherInfo;
+      })
+      .catch((e) => {
         console.error(e.message);
-    }
-    }
-    return { getJSONData };
+      });
+  }
+
+  return { getJSONData, processWeather };
 })();
