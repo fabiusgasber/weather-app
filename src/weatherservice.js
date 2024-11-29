31deaf288f288a0bc928ba2ac01b11e9c7421645
Fabius Gasber <fabiusgasber@gmail.com>
2024-11-29 13:05:32 +0100

    chore: code style changes with prettier

--- src/weatherservice.js
+++ src/weatherservice.js
@@ -1,6 +1,17 @@
 export const weatherservice = (() => {
-    async function getJSONData (location) {
+  async function getJSONData(location) {
     try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?IconSet=icons1&key=W8SRDE35NC9EZSAB2UEM3ZD9H`,
+      );
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+      return await response.json();
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+
   function processWeather(json) {
     if (!json || !(json instanceof Promise))
       throw new Error("Data error! Unexpected data format");
