15f5c97bd3987eb6f1d15589956c4e87559638e9
Fabius Gasber <fabiusgasber@gmail.com>
2024-12-03 13:45:01 +0100

    feat: create UnitHandler class to handle different unit systems

---
+++ src/units.js
@@ -0,0 +1,21 @@
+class UnitHandler {
+    constructor(system = {}){
+        this.system = system;
+    }
+
+    getUnit = (key) => this.system[key] || "";
+    addUnit = (key, value) => this.system[key] = value;
+    formatUnit = (key, value) => `${value} ${this.getUnit(key)}`
+}
+
+export const metricSystem = new UnitHandler({
+    temperature: "°C",
+    precipitation: "%",
+    windspeed: "km/h",
+  });
+
+export const imperialSystem = new UnitHandler({
+    temperature: "°F",
+    precipitation: "%",
+    windspeed: "mph",
+  });
